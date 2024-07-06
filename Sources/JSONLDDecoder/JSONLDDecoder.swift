c91f86aa71bbb7888354cacb324e87abd7c00224
ausra <ausra.balyne@gmail.com>
2024-07-06 18:35:09 +0300

    generic json decoder init

--- Sources/JSONLDDecoder/JSONLDDecoder.swift
+++ Sources/JSONLDDecoder/JSONLDDecoder.swift
@@ -1,2 +1,13 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public struct RecipeJSONLDDecoder {
+    private let decoder: JSONDecoder
+
+    public init(decoder: JSONDecoder = JSONDecoder()) {
+        self.decoder = decoder
+    }
+
+    public func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
+        return try decoder.decode(T.self, from: data)
+    }
+}
--- Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
+++ Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
@@ -1,6 +1,35 @@
 import Testing
+import Foundation
 @testable import JSONLDDecoder
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct Person: Decodable {
+    let name: String
+    let age: Int
 }
+
+let jsonData = """
+        {
+            "name": "John Doe",
+            "age": 30
+        }
+        """.data(using: .utf8)!
+
+@Test("Return name", arguments: [
+    jsonData
+])
+
+func testDecoder(jsonData: Data) async throws {
+    let decoder = RecipeJSONLDDecoder()
+
+    do {
+        let person = try decoder.decode(Person.self, from: jsonData)
+        #expect(person.name == "John Doe")
+        #expect(person.age == 30)
+    } catch {
+        Issue.record("Decoding failed with error: \(error)")
+    }
+
+}
+
+
+
