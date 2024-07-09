f09f1bcd269d47d2dc518dce71a09c342c607fba
ausra <ausra.balyne@gmail.com>
2024-07-09 08:33:54 +0300

    moved decoding to property wrapper so that first the attepts is to decode single value, and if it fails the nested coding keys decoded

--- Sources/JSONLDDecoder/JSONLDDecoder.swift
+++ Sources/JSONLDDecoder/JSONLDDecoder.swift
@@ -16,32 +16,40 @@ public struct RecipeJSONLDDecoder {
 @propertyWrapper
 public struct NestedDecodable<T: Decodable, Keys: CodingKey & CaseIterable>: Decodable {
     public var wrappedValue: T
+
     public init(wrappedValue: T) {
         self.wrappedValue = wrappedValue
     }
-}
 
-enum ValidationError: Error {
-    case noNestedKeys
-}
-
-extension KeyedDecodingContainer {
-    func decode<T, NestedKeys>(_: NestedDecodable<T, NestedKeys>.Type, forKey key: Key) throws -> NestedDecodable<T, NestedKeys> {
-        guard NestedKeys.allCases.isEmpty == false else { throw ValidationError.noNestedKeys }
-
-        var container = try self.nestedContainer(keyedBy: NestedKeys.self, forKey: key)
-        for key in NestedKeys.allCases.dropLast() {
-            container = try container.nestedContainer(keyedBy: NestedKeys.self, forKey: key)
+    public init(from decoder: Decoder) throws {
+        // Attempt to decode the value as a direct string
+        if let singleValueContainer = try? decoder.singleValueContainer(), let value = try? singleValueContainer.decode(T.self) {
+            self.wrappedValue = value
+        } else {
+            // If direct decoding fails, attempt to decode as a nested container
+            let container = try decoder.container(keyedBy: Keys.self)
+            self.wrappedValue = try NestedDecodable.decodeFromNestedContainer(container, nestedKeysType: Keys.self)
         }
-        let wrappedValue = try container.decode(T.self, forKey: NestedKeys.lastCase!)
-        return NestedDecodable(wrappedValue: wrappedValue)
+
+    }
+
+    private static func decodeFromNestedContainer<NestedKeys: CodingKey & CaseIterable>(_ container: KeyedDecodingContainer<NestedKeys>, nestedKeysType: NestedKeys.Type) throws -> T {
+        let allKeys = Array(NestedKeys.allCases)
+        guard let firstKey = allKeys.first else {
+            throw DecodingError.dataCorruptedError(forKey: allKeys.first!, in: container, debugDescription: "No nested keys found.")
+        }
+
+        var nestedContainer = container
+        for key in allKeys.dropLast() {
+            nestedContainer = try nestedContainer.nestedContainer(keyedBy: NestedKeys.self, forKey: key)
+        }
+
+        if let lastKey = allKeys.last {
+            return try nestedContainer.decode(T.self, forKey: lastKey)
+        }
+
+        throw DecodingError.dataCorruptedError(forKey: firstKey, in: nestedContainer, debugDescription: "Missing expected key for nested decoding.")
     }
 }
 
-extension CaseIterable {
-    static var lastCase: Self? {
-        guard allCases.isEmpty == false else { return nil }
-        let lastIndex = allCases.index(allCases.endIndex, offsetBy: -1)
-        return allCases[lastIndex]
-    }
-}
+
--- Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
+++ Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
@@ -6,48 +6,63 @@ struct Recipe: Decodable {
     var name: String?
     @NestedDecodable<String?, AuthorCodingKeys>
     var author: String?
-
+    @NestedDecodable<String?, DescriptionCodingKeys>
+    var description: String?
+    // nested enums to decoded
     enum AuthorCodingKeys: String, CodingKey, CaseIterable {
         case name
     }
+
+    enum DescriptionCodingKeys: String, CodingKey, CaseIterable {
+        case description
+    }
 }
 
-let jsonPersonData = """
+let jsonDataWithStrings = """
 {
     "name": "Cupcakes",
-    "author": { "@type": "Person", "name": "Mary Cope" },
-    "notRelavant": "nothing"
+    "author": "Mary Cope",
+    "notRelavant": "nothing",
+    "description": "something"
 }
 """.data(using: .utf8)!
 
-let jsonOrganizationData = """
+let jsonDataWithObjects = """
 {
     "name": "Cupcakes",
      "author": {
         "@type": "Organization",
         "name": "Bananaland"
       },
-    "notRelavant": "nothing"
+    "notRelavant": "nothing",
+    "description": {
+        "something": "some",
+        "description": "amazing"
+}
 }
 """.data(using: .utf8)!
 
-@Test("Return nested author name", arguments: [
-    jsonPersonData, jsonOrganizationData
+@Test("Returns nested strings", arguments: [
+    jsonDataWithStrings, jsonDataWithObjects
 ])
 
 func testDecoder(jsonData: Data) async throws {
     let decoder = RecipeJSONLDDecoder()
 
     do {
-        let recipe = try decoder.decode(Recipe.self, from: jsonPersonData)
+        let recipe = try decoder.decode(Recipe.self, from: jsonDataWithStrings)
+        #expect(recipe.name == "Cupcakes")
         #expect(recipe.author == "Mary Cope")
+        #expect(recipe.description == "something")
     } catch {
         Issue.record("Decoding failed with error: \(error)")
     }
 
     do {
-        let recipe = try decoder.decode(Recipe.self, from: jsonOrganizationData)
+        let recipe = try decoder.decode(Recipe.self, from: jsonDataWithObjects)
+        #expect(recipe.name == "Cupcakes")
         #expect(recipe.author == "Bananaland")
+        #expect(recipe.description == "amazing")
     } catch {
         Issue.record("Decoding failed with error: \(error)")
     }
