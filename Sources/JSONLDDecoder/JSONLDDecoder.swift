3d668dc3e0a6af0ab2ebea2dc3f42f53e0a964db
ausra <ausra.balyne@gmail.com>
2024-07-13 16:54:55 +0300

    made keydedcontainer function public

--- Sources/JSONLDDecoder/JSONLDDecoder.swift
+++ Sources/JSONLDDecoder/JSONLDDecoder.swift
@@ -150,12 +150,12 @@ public struct ArrayOfNestedObjectsDecoder<T: Decodable, Keys: CodingKey & CaseIt
 }
 
 extension KeyedDecodingContainer {
-    func decode(_ type: StringArrayDecoder.Type, forKey key: Key) throws -> StringArrayDecoder {
-        if let stringValue = try? decode(String.self, forKey: key) {
+    public func decode(_ type: StringArrayDecoder.Type, forKey key: Key) throws -> StringArrayDecoder {
+        if let stringValue = try? decodeIfPresent(String.self, forKey: key) {
             return StringArrayDecoder(wrappedValue: [stringValue])
-        } else if let arrayValue = try? decode([String].self, forKey: key) {
+        } else if let arrayValue = try? decodeIfPresent([String].self, forKey: key) {
             return StringArrayDecoder(wrappedValue: arrayValue)
-        } else if let intValue = try? decode(Int.self, forKey: key) {
+        } else if let intValue = try? decodeIfPresent(Int.self, forKey: key) {
             return StringArrayDecoder(wrappedValue: [String(intValue)])
         } else {
             return StringArrayDecoder(wrappedValue: nil)
