db6735d53c69fd9f040e0861eba314d61f96bf3f
ausra <ausra.balyne@gmail.com>
2024-07-15 20:13:23 +0300

    addaptive array decoder

--- Sources/JSONLDDecoder/JSONLDDecoder.swift
+++ Sources/JSONLDDecoder/JSONLDDecoder.swift
@@ -61,6 +61,8 @@ public struct NestedObjectsDecoder<T: Decodable, Keys: CodingKey & CaseIterable>
     }
 }
 
+
+
 @propertyWrapper
 public struct ArrayOfNestedObjectsDecoder<T: Decodable, Keys: CodingKey & CaseIterable>: Decodable {
     public var wrappedValue: [T]?
@@ -134,7 +136,7 @@ public struct ArrayOfNestedObjectsDecoder<T: Decodable, Keys: CodingKey & CaseIt
         return result
     }
     
-    struct DynamicCodingKey: CodingKey {
+     struct DynamicCodingKey: CodingKey {
         var stringValue: String
         init?(stringValue: String) {
             self.stringValue = stringValue
@@ -149,6 +151,28 @@ public struct ArrayOfNestedObjectsDecoder<T: Decodable, Keys: CodingKey & CaseIt
     
 }
 
+@propertyWrapper
+public struct AdaptiveArrayDecoder<T: NestedObjectProtocol & Decodable>: Decodable {
+    public var wrappedValue: [T]?
+
+    public init(wrappedValue: [T]?) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+public protocol NestedObjectProtocol: Decodable {
+    var text: String? { get }
+    init(text: String?)
+}
+
+public struct NestedObject: NestedObjectProtocol, Decodable {
+    public var text: String?
+
+    public init(text: String?) {
+        self.text = text
+    }
+}
+
 extension KeyedDecodingContainer {
     public func decode(_ type: StringArrayDecoder.Type, forKey key: Key) throws -> StringArrayDecoder {
         if let stringValue = try? decodeIfPresent(String.self, forKey: key) {
@@ -159,6 +183,21 @@ extension KeyedDecodingContainer {
             return StringArrayDecoder(wrappedValue: [String(intValue)])
         } else {
             return StringArrayDecoder(wrappedValue: nil)
+        }
+    }
+   
+    public func decode<T: NestedObjectProtocol & Decodable>(_ type: AdaptiveArrayDecoder<T>.Type, forKey key: Key) throws -> AdaptiveArrayDecoder<T> {
+        if let singleValue = try? decodeIfPresent(String.self, forKey: key) {
+            let nestedObject = T.init(text: singleValue)
+            return AdaptiveArrayDecoder(wrappedValue: [nestedObject])
+        } else if let arrayOfStringValues = try? decodeIfPresent([String].self, forKey: key) {
+            let arrayOfNestedObjects = arrayOfStringValues.map { T.init(text: $0) }
+            return AdaptiveArrayDecoder(wrappedValue: arrayOfNestedObjects)
+        } else if let nestedObjects = try? decode([T].self, forKey: key) {
+            return AdaptiveArrayDecoder(wrappedValue: nestedObjects)
+        }
+        else {
+            return AdaptiveArrayDecoder(wrappedValue: nil)
         }
     }
 }
--- Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
+++ Tests/JSONLDDecoderTests/JSONLDDecoderTests.swift
@@ -7,55 +7,55 @@ import Foundation
         @StringArrayDecoder
         var recipeYield: [String]?
     }
-    
+
     let jsonInt = """
 {
     "recipeYield": 0
 }
 """.data(using: .utf8)!
-    
+
     let jsonArray = """
 {
     "recipeYield": ["6", "6 patties"]
 }
 """.data(using: .utf8)!
-    
+
     let jsonString = """
 {
     "recipeYield": "6"
 }
 """.data(using: .utf8)!
-    
+
     let jsonMissingKey = """
 {
     "name": "6"
 }
 """.data(using: .utf8)!
-    
+
     @Test("Returns array of strings") func testDecoder() async throws {
         let decoder = RecipeJSONLDDecoder()
-        
+
         do {
             let recipe = try decoder.decode(RecipeYield.self, from: jsonInt)
             #expect(recipe.recipeYield == ["0"])
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeYield.self, from: jsonArray)
             #expect(recipe.recipeYield == ["6", "6 patties"])
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeYield.self, from: jsonString)
             #expect(recipe.recipeYield == ["6"])
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeYield.self, from: jsonMissingKey)
             #expect(recipe.recipeYield == nil)
@@ -63,32 +63,32 @@ import Foundation
             Issue.record("Decoding failed with error: \(error)")
         }
     }
-    
+
 }
 
 @Suite("ArrayOfNestedObjectsDecoderTests") struct ArrayOfNestedObjectsDecoderTests {
     struct RecipeImages: Decodable {
-        
+
         @ArrayOfNestedObjectsDecoder<String, ImageCodingKeys>
         var images: [String]?
-        
+
         enum CodingKeys: String, CodingKey {
             case images = "image"
         }
-        
+
         enum ImageCodingKeys: String, CodingKey, CaseIterable {
             case url
         }
-        
+
     }
-    
+
     let jsonImageString = """
 {
     "image": "https://www.images.com/",
 
 }
 """.data(using: .utf8)!
-    
+
     let jsonImageArray = """
 {
      "image": [
@@ -97,7 +97,7 @@ import Foundation
       ],
 }
 """.data(using: .utf8)!
-    
+
     let jsonImageObjectsArray = """
 {
     "image": [
@@ -112,7 +112,7 @@ import Foundation
     ]
 }
 """.data(using: .utf8)!
-    
+
     let jsonImageObject =  """
 {
     "image":
@@ -122,7 +122,7 @@ import Foundation
     }
 }
 """.data(using: .utf8)!
-    
+
     @Test("Returns an array of strings") func testDecoder() async throws {
         let decoder = RecipeJSONLDDecoder()
         do {
@@ -131,7 +131,7 @@ import Foundation
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeImages.self, from: jsonImageArray)
             #expect(recipe.images == [
@@ -141,7 +141,7 @@ import Foundation
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeImages.self, from: jsonImageObject)
             #expect(recipe.images == [
@@ -150,7 +150,7 @@ import Foundation
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeImages.self, from: jsonImageObjectsArray)
             #expect(recipe.images == [
@@ -165,16 +165,16 @@ import Foundation
 
 @Suite("NestedObjectsDecoderTests")
 struct NestedObjectsDecoderTests {
-    
+
     struct RecipeAuthor: Decodable {
         @NestedObjectsDecoder<String?, AuthorCodingKeys>
         var author: String?
-        
+
         enum AuthorCodingKeys: String, CodingKey, CaseIterable {
             case name
         }
     }
-    
+
     let jsonObject =  """
 {
     "author":
@@ -184,23 +184,23 @@ struct NestedObjectsDecoderTests {
     }
 }
 """.data(using: .utf8)!
-    
+
     let jsonString =  """
 {
     "author": "John Adam"
 }
 """.data(using: .utf8)!
-    
+
     @Test func testDecoder() async throws {
         let decoder = RecipeJSONLDDecoder()
-        
+
         do {
             let recipe = try decoder.decode(RecipeAuthor.self, from: jsonObject)
             #expect(recipe.author == "John Adam")
         } catch {
             Issue.record("Decoding failed with error: \(error)")
         }
-        
+
         do {
             let recipe = try decoder.decode(RecipeAuthor.self, from: jsonString)
             #expect(recipe.author == "John Adam")
@@ -210,3 +210,123 @@ struct NestedObjectsDecoderTests {
     }
 }
 
+@Suite("AdaptiveArrayDecoderTests")
+struct AdaptiveArrayDecoderTests {
+
+    struct RecipeInstructions: Decodable {
+
+        @AdaptiveArrayDecoder
+        var instructions: [ParsedInstruction]?
+
+        enum CodingKeys: String, CodingKey {
+            case instructions = "recipeInstructions"
+        }
+    }
+
+    struct ParsedInstruction: Decodable, Equatable, NestedObjectProtocol {
+        var text: String?
+        var name: String?
+        var image: String?
+
+        private enum CodingKeys: String, CodingKey {
+            case name, text, image = "url"
+        }
+
+        init(text: String?, name: String? = nil, image: String? = nil) {
+            self.text = text
+            self.name = name
+            self.image = image
+        }
+
+        init(text: String?) {
+            self.text = text
+            self.name = nil
+            self.image = nil
+        }
+    }
+
+    let jsonHowToObjects = """
+    {
+         "recipeInstructions": [
+            {
+              "@type": "HowToStep",
+              "text": "step 1",
+              "name": "step 1",
+              "url": "https://worldrecipes.com/#step-1"
+            },
+            {
+              "@type": "HowToStep",
+              "text": "Step 2",
+              "name": "Step 2",
+              "url": "https://worldrecipes.com/#step-2"
+            }
+        ]
+    }
+    """.data(using: .utf8)!
+
+    let jsonSingleString = """
+    {
+        "recipeInstructions": "Step1 Step2"
+    }
+    """.data(using: .utf8)!
+
+    let jsonArrayOfStrings = """
+    {
+        "recipeInstructions": ["Step1", "Step2"]
+    }
+    """.data(using: .utf8)!
+
+    let jsonNullString = """
+        {
+            "recipeInstructions": null
+        }
+    """.data(using: .utf8)!
+
+    @Test("Returns an array of decoded objects from a single string") func testSingleStringDecoder() async throws {
+        let decoder = RecipeJSONLDDecoder()
+        do {
+            let recipe = try decoder.decode(RecipeInstructions.self, from: jsonSingleString)
+            #expect(recipe.instructions == [ParsedInstruction(text: "Step1 Step2", name: nil, image: nil)])
+        } catch {
+            Issue.record("Decoding failed with error: \(error)")
+        }
+    }
+
+    @Test("Returns an array of decoded objects from an array of strings") func testArrayOfStringsDecoder() async throws {
+        let decoder = RecipeJSONLDDecoder()
+        do {
+            let recipe = try decoder.decode(RecipeInstructions.self, from: jsonArrayOfStrings)
+            #expect(recipe.instructions == [
+                ParsedInstruction(text: "Step1", name: nil, image: nil),
+                ParsedInstruction(text: "Step2", name: nil, image: nil)
+            ])
+        } catch {
+            Issue.record("Decoding failed with error: \(error)")
+        }
+    }
+
+    @Test("Returns an array of decoded objects from an array of objects") func testArrayOfObjectsDecoder() async throws {
+        let decoder = RecipeJSONLDDecoder()
+
+        do {
+            let recipe = try decoder.decode(RecipeInstructions.self, from: jsonHowToObjects)
+            #expect(recipe.instructions == [
+                ParsedInstruction(text: "step 1", name: "step 1", image: "https://worldrecipes.com/#step-1"),
+                ParsedInstruction(text: "Step 2", name: "Step 2", image: "https://worldrecipes.com/#step-2")
+            ])
+        } catch {
+            Issue.record("Decoding failed with error: \(error)")
+        }
+    }
+
+    @Test("Returns a nil value") func testNilDecoder() async throws {
+        let decoder = RecipeJSONLDDecoder()
+
+        do {
+            let recipe = try decoder.decode(RecipeInstructions.self, from: jsonNullString)
+            #expect(recipe.instructions == nil)
+        } catch {
+            Issue.record("Decoding failed with error: \(error)")
+        }
+    }
+}
